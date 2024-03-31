d55a68f4bde058a551998b9bb787216ea7a59289
Domantas1337 <domantas.kakc@gmail.com>
2024-03-31 14:32:05 +0300

    Changed collision logic, removed if conditions

--- untitled/src/helpClass/collision.java
+++ untitled/src/helpClass/collision.java
@@ -5,32 +5,26 @@ import main.Game;
 import java.awt.geom.Rectangle2D;
 
 public class collision {
+
 	public static boolean CanMoveHere(float x, float y, float width, float height, int[][] lvlData) {
-		if (!IsSolid(x, y, lvlData))
-			if (!IsSolid(x + width, y + height, lvlData))
-				if (!IsSolid(x + width, y, lvlData))
-					if (!IsSolid(x, y + height, lvlData))
-						return true;
-		return false;
+		// Check if any of the corners are in a solid tile. If none are, movement is possible.
+		return !(IsSolid(x, y, lvlData) ||
+				IsSolid(x + width, y, lvlData) ||
+				IsSolid(x, y + height, lvlData) ||
+				IsSolid(x + width, y + height, lvlData));
 	}
 
 	private static boolean IsSolid(float x, float y, int[][] lvlData) {
-		if (x < 0 || x >= Game.GAME_WIDTH)
-			return true;
-		if (y < 0 || y >= Game.GAME_HEIGHT)
+		if (x < 0 || x >= Game.GAME_WIDTH || y < 0 || y >= Game.GAME_HEIGHT)
 			return true;
 
+		int xIndex = (int) (x / Constants.PlayerConstants.TILES_SIZE);
+		int yIndex = (int) (y / Constants.PlayerConstants.TILES_SIZE);
 
-		float xIndex = x /  Constants.PlayerConstants.TILES_SIZE;
-		float yIndex = y /  Constants.PlayerConstants.TILES_SIZE;
+		int value = lvlData[yIndex][xIndex];
 
-		int value = lvlData[(int) yIndex][(int) xIndex];
-
-		if (value == 0  || value == 2 || value < 0 ){
-			return true;
-		}
-
-		return false;
+		// Assuming the interpretation is that 0, 2, and negative values are considered solid.
+		return value == 0 || value == 2 || value < 0;
 	}
 
 	public static float GetEntityXPosNextToWall(Rectangle2D.Float hitbox, float xSpeed) {
